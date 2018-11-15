e11bcafe37b3eb77f27b4d180b13d9b025e44c3c
994108345 <994108345@qq.com>
2018-11-15 20:25:35 +0800

    搭好环境

--- sbc-pay/sbc-pay-common/src/main/java/cn/wzl/sbc/pay/common/util/Md5Util.java
+++ sbc-pay/sbc-pay-common/src/main/java/cn/wzl/sbc/pay/common/util/Md5Util.java
@@ -1,17 +1,13 @@
 package cn.wzl.sbc.pay.common.util;
 
-import sun.misc.BASE64Encoder;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class Md5Util {
-    public static  String mod5(String data) throws NoSuchAlgorithmException {
+    /*public static  String mod5(String data) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("md5");
         byte[] md5 = md.digest(data.getBytes());
         BASE64Encoder be = new BASE64Encoder();
         String base64 = be.encode(md5);
         System.out.println(base64);
         return base64;
-    }
+    }*/
 }
